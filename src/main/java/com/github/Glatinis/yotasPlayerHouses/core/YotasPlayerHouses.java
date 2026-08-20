02430772d661a05da561c585142a8fd232173570
Glatinis <63927852+Glatinis@users.noreply.github.com>
2026-08-20 23:07:58 +0800

    Wire up and add fixes

--- src/main/java/com/github/Glatinis/yotasPlayerHouses/core/YotasPlayerHouses.java
+++ src/main/java/com/github/Glatinis/yotasPlayerHouses/core/YotasPlayerHouses.java
@@ -1,13 +1,17 @@
 package com.github.Glatinis.yotasPlayerHouses.core;
 
+import com.github.Glatinis.yotasPlayerHouses.world.HouseWorldManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class YotasPlayerHouses extends JavaPlugin {
     private ConfigManager configManager;
+    private HouseWorldManager houseWorldManager;
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
         configManager = new ConfigManager(this);
+        houseWorldManager = new HouseWorldManager(this, configManager);
 
         getLogger().info("Loaded plugin!");
     }
