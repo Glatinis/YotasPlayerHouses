7a5567f0007222f968ab072bb2f1d06e909b98c2
Glatinis <63927852+Glatinis@users.noreply.github.com>
2026-08-20 23:14:00 +0800

    Added Multiverse-Core dependency check

--- src/main/java/com/github/Glatinis/yotasPlayerHouses/core/YotasPlayerHouses.java
+++ src/main/java/com/github/Glatinis/yotasPlayerHouses/core/YotasPlayerHouses.java
@@ -1,6 +1,7 @@
 package com.github.Glatinis.yotasPlayerHouses.core;
 
 import com.github.Glatinis.yotasPlayerHouses.world.HouseWorldManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class YotasPlayerHouses extends JavaPlugin {
@@ -9,6 +10,12 @@ public final class YotasPlayerHouses extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        if (!Bukkit.getPluginManager().isPluginEnabled("Multiverse-Core")) {
+            getLogger().severe("Multiverse-Core version 5.8.0 is required to run this plugin!");
+            getLogger().severe("Unloading...");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+
         saveDefaultConfig();
         configManager = new ConfigManager(this);
         houseWorldManager = new HouseWorldManager(this, configManager);
