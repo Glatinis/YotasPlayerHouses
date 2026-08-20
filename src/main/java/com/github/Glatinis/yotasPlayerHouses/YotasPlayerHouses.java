c8f7d650d3edd233d0cd3a807249ddd89afdaada
Glatinis <63927852+Glatinis@users.noreply.github.com>
2026-08-20 22:10:22 +0800

    Initialize

---
+++ src/main/java/com/github/Glatinis/yotasPlayerHouses/YotasPlayerHouses.java
@@ -0,0 +1,16 @@
+package com.github.Glatinis.yotasPlayerHouses;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class YotasPlayerHouses extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
