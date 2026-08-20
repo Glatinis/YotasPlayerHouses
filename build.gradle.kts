e17849027a662c964dc7992794c7d18eef4988da
Glatinis <63927852+Glatinis@users.noreply.github.com>
2026-08-20 22:20:48 +0800

    Add Multiverse Core dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,12 @@ plugins {
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.onarandombox.com/content/groups/public/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("org.mvplugins.multiverse.core:multiverse-core:5.8.0")
 }
 
 java {
