3071057c2d8999fbbd6ea55ccbafcc04259d5e68
Glatinis <63927852+Glatinis@users.noreply.github.com>
2026-08-23 22:53:20 +0800

    Add FAWE and Vault dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,18 @@ repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.onarandombox.com/content/groups/public/")
+    maven("https://maven.enginehub.org/repo/")
+    maven("https://jitpack.io")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     compileOnly("org.mvplugins.multiverse.core:multiverse-core:5.8.0")
+    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core:2.14.2")
+    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit:2.14.2") {
+        isTransitive = false
+    }
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
 }
 
 java {
